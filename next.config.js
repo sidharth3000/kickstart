4c5b76525db30c227a6e3214f74d3ae48ca36552
Jatin Gupta <201951073@iiitvadodara.ac.in>
2021-11-09 01:21:56 +0530

    error fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
+      "/campaigns/new": { page: "/campaigns/new" },
+      "/campaigns/show": { page: "/campaigns/show" },
+      "/campaigns/requests/": { page: "/campaigns/requests/" },
+      "/campaigns/requests/new": { page: "/campaigns/requests/new" },
     };
   },
 };
